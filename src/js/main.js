3188fa2366021ccb103b7290bf9f55450a16861a
MerlinDS <merlinkolv@gmail.com>
2015-09-30 22:49:02 +0300

    fix

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-var currentColorTheme = 'vanilla-theme';
-
-function changeColorTheme(themeId){
-    if(currentColorTheme != themeId)
-    {
-        document.getElementById('color-theme').setAttribute('href', 'css/' + themeId + '.css');
-        currentColorTheme = themeId;
-    }
-}
